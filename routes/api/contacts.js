5cf23d0e51a37b3464cc782b2f96f5657d8e0024
Liliia Shemet <lilka8069@gmail.com>
2022-01-02 14:27:04 +0200

    add hw 03 with using mongodb and mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,91 +1,15 @@
-const express = require('express')
-const { NotFound, BadRequest } = require('http-errors')
-const Joi = require('joi')
-const router = express.Router()
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  updateContact,
-  removeContact,
-} = require('../../model/index')
+const express = require('express');
+const router = express.Router();
+const contactsController = require('../../controller');
+const { validateContact } = require('../../service/validation');
 
-const joiShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-}).min(1)
+router
+  .get('/', contactsController.listContacts)
+  .post('/', validateContact, contactsController.addContact);
 
-router.get('/', async (req, res, next) => {
-  try {
-    res.json(await listContacts())
-  } catch (error) {
-    next(error)
-  }
-})
+router
+  .get('/:contactId', contactsController.getContactById)
+  .delete('/:contactId', contactsController.removeContact)
+  .patch('/:contactId', validateContact, contactsController.updateContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  try {
-    const contacts = await getContactById(contactId)
-    if (!contacts) {
-      throw new NotFound()
-    }
-    res.json(contacts)
-  } catch (error) {
-    next(error)
-  }
-})
-router.post('/', async (req, res, next) => {
-  const body = req.body
-  try {
-    const { error } = joiShema.validate(body)
-    if (error) {
-      throw new BadRequest('missing required name field')
-      // const error = new Error('missing required name field')
-      // error.status = 400
-      // throw error
-    }
-    const contacts = await addContact(body)
-    res.status(201).json(contacts)
-    console.log(req.body)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const deleteContact = await removeContact(contactId)
-    if (!deleteContact) {
-      throw new NotFound()
-    }
-    res.json({ message: 'contact deleteds' })
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.patch('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = joiShema.validate(req.body)
-    if (error) {
-      throw new BadRequest('missing fields')
-      // const error = new Error('missing fields')
-      // error.status = 400
-      // throw error
-    }
-    const { contactId } = req.params
-    const updateContactById = await updateContact(contactId, req.body)
-    if (updateContactById) {
-      res.json(updateContactById)
-    } else {
-      throw new NotFound()
-    }
-  } catch (error) {
-    next(error)
-  }
-})
-
-module.exports = router
+module.exports = router;
